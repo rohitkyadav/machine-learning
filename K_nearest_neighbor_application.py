7445f1ba4c556578040d6700c35db4760fe9a5c7
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-07-23 14:50:36 +0530

    K nearest neighbor application

--- K_nearest_neighbor_application.py
+++ K_nearest_neighbor_application.py
@@ -2,23 +2,28 @@ import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
+accuracies = []
+for i in range(5):
+	df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+	df.replace('?', -99999, inplace=True)
+	df.drop(['id'], 1, inplace=True)
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+	X = np.array(df.drop(['class'], 1))
+	y = np.array(df['class'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+	clf = neighbors.KNeighborsClassifier()
+	clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+	accuracy = clf.score(X_test, y_test)
+	# print(accuracy)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)  # len(example_measures) denotes number of dataset i.e patients
+	# example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
+	# example_measures = example_measures.reshape(len(example_measures),-1)  # len(example_measures) denotes number of dataset i.e patients
 
-prediction = clf.predict(example_measures)
-print(prediction)
+	# prediction = clf.predict(example_measures)
+	# print(prediction)
+
+	accuracies.append(accuracy)
+print(sum(accuracies)/len(accuracies))
