a6c0dc901faa7da51eedb49d2bb35c5a43b15706
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-08-04 23:04:04 +0530

    support vector algorithm

--- SVA.py
+++ SVA.py
@@ -50,19 +50,44 @@ class Suppert_Vector_machine:
 			# we can do this because convex
 			optimized = False
 			while not optimized:
-				pass
+				for b in np.arange(-1*(self.max_feature_value*b_range_multiple), 
+									self.max_feature_value*b_range_multiple, 
+									step*b_multiple):
+					for transformation in transforms:
+						w_t = w*transformation
+						found_option = True
+						# weakest link in the SVM fundamentally
+						# SMO attempts to fix this a bit
+						# yi(xi.w+b) >= 1
+						for i in self.data:
+							for xi in self.data[i]:
+								yi = i
+								if not yi*(np.dot(w_t, xi) + b) >= 1:
+									found_option = False
+						
+						if found_option:
+							opt_dict[np.linalg.norm(w_t)] = [w_t, b]
 
+				if w[0] < 0:
+					optimized = True
+					print('optimised a step:')
+				else:
+					w = w - step
 
-
-
-
+			norms = sorted([n for n in opt_dict])
+			
+			opt_choice = opt_dict[norms[0]]
+			self.w = opt_choice[0]
+			self.b = opt_choice[1]
+			latest_optimum = opt_choice[0][0]+step*2
 
 	def predict(self, data):
 		# sign( x.w+b )
 		classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+		if classification !=0 and self.visualization:
+			self
+
 		return classification
-
-
 
 data_dict = {-1:np.array([[1,7], [2,8], [3,8],]),
 			  1:np.array([[5,1], [6,-1], [7,3],]),
