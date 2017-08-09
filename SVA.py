ec985d61ffc0993139d1f9619118de02b61c2d7b
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-08-09 23:11:17 +0530

    support vector algorithm

--- SVA.py
+++ SVA.py
@@ -7,7 +7,7 @@ import numpy as np
 
 style.use('ggplot')
 
-class Suppert_Vector_machine:
+class Support_Vector_Machine:
 	def __init__(self, visualization=True):
 		self.visualization = visualization
 		self.color = {1:'r', -1:'b'}
@@ -85,10 +85,50 @@ class Suppert_Vector_machine:
 		# sign( x.w+b )
 		classification = np.sign(np.dot(np.array(features), self.w) + self.b)
 		if classification !=0 and self.visualization:
-			self
-
+			self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
 		return classification
 
-data_dict = {-1:np.array([[1,7], [2,8], [3,8],]),
-			  1:np.array([[5,1], [6,-1], [7,3],]),
-}
+	def visualize(self):
+		[[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+		# hyperplane = x.w+b
+		# v = x.w+b
+		# psv = 1
+		# nsv = -1
+		# dec = 0
+		def hyperplane(x,w,b,v):
+			return (-w[0]*x-b+v) / w[1]
+
+		datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+		hyp_x_min = datarange[0]
+		hyp_x_max = datarange[1]
+
+		# (w.x+b) = 1
+		# positive support vector hyperplane
+		psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+		psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+		self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+
+		# (w.x+b) = -1
+		# negative support vector hyperplane
+		nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+		nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+		self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+
+		# (w.x+b) = 0
+		# positive support vector hyperplane
+		db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+		db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+		self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+
+		plt.show()
+
+data_dict = {
+				-1:np.array([[1,7], [2,8], [3,8],]),
+				1:np.array([[5,1], [6,-1], [7,3],]),
+			}
+
+
+svm = Support_Vector_Machine()
+svm.fit(data = data_dict)
+svm.visualize()
